72bcf98098615ad287f1249aa9a9f6f5375b3267
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-09-13 21:18:08 +0100

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var main = function() {
+    //toggle <ul class="more-menu">
+    $('.more-btn').on('click', ({currentTarget}) => {
+        $(currentTarget).text() === 'More' && $(currentTarget).text('Less') || $(currentTarget).text('More')
+        $(currentTarget).next().toggle()
+    })
+    
+    //toggle share menu"
+    $('.share').on('click', ({currentTarget}) => {
+        $(currentTarget).next().toggle()
+    })
 
+    //
+    $('.bell').on('click', ({currentTarget}) => $(currentTarget).toggleClass('notification active'))
 };
 
 $(document).ready(main);
